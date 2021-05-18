2f6f7018540f70dffc9ff5ebd211c8405c2e2ed4
Đoàn Thế Vinh <vinhdt@teky.edu.vn>
2021-05-18 13:10:35 +0700

    Update README
    Add ClassIn documentation link

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# README
+# ClassIn statistic APIs
 
 Receive Data from ClassIn and provide API for other systems.
+
+
+### References
+* [EEO Documentation](https://docs.eeo.cn/api/en/datasub/schoolrelated.html)
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+# lock "~> 3.14.1"
 
 set :application, "classin.codable.vn"
 set :repo_url, "git@gitlab.com:teky.vn/classin-statistic-data.git"
